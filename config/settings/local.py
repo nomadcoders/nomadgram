e6cf70b6de13e554b325a495d2fcf4463680e058
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-12 14:49:03 +0700

    Pulled readme

---
+++ config/settings/local.py
@@ -0,0 +1,65 @@
+"""
+Local settings
+
+- Run in Debug mode
+
+- Use mailhog for emails
+
+- Add Django Debug Toolbar
+- Add django-extensions as app
+"""
+
+from .base import *  # noqa
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='.80h]pfMpBKYeMmBUKB`[YN$7q*7^y<284RUt#zq[eWJG[]k&c')
+
+# Mail settings
+# ------------------------------------------------------------------------------
+
+EMAIL_PORT = 1025
+
+EMAIL_HOST = 'localhost'
+
+
+# CACHING
+# ------------------------------------------------------------------------------
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+INSTALLED_APPS += ['debug_toolbar', ]
+
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+
+# django-extensions
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ['django_extensions', ]
+
+# TESTING
+# ------------------------------------------------------------------------------
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Your local stuff: Below this line define 3rd party library settings
+# ------------------------------------------------------------------------------
