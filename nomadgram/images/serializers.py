ff83103f8d1996de22d08b83bd14f21171f2d038
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-13 22:02:49 +0700

    Serializing Foreing Keys

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -11,12 +11,16 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    image = ImageSerializer()
+
     class Meta:
         model = models.Comment
         fields = '__all__'
 
 
 class LikeSerializer(serializers.ModelSerializer):
+
+    image = ImageSerializer()
 
     class Meta:
         model = models.Like
