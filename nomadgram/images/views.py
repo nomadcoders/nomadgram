a66372d3de4db50abe0ccc0f7e2712db95098ffb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-15 17:31:18 +0700

    Including myself on the feed

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -23,6 +23,12 @@ class Feed(APIView):
 
                 image_list.append(image)
 
+        my_images = user.images.all()[:2]
+
+        for image in my_images:
+
+            image_list.append(image)
+
         sorted_list = sorted(
             image_list, key=lambda image: image.created_at, reverse=True)
 
