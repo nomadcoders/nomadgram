a484dd26f73aded6b236f3ccdc202f4e056b5d0c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-31 14:58:10 +0700

    Creating the User Reducer

---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,28 @@
+// imports
+
+// actions
+
+// action creators
+
+// initial state
+
+// reducer
+
+const initialState = {
+  isLoggedIn: false
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// reducer functions
+
+// exports
+
+// export reducer by default
+
+export default reducer;
