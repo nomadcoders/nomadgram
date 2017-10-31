2a95b65ae7e83feeb084d21b43cf12fead364868
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-31 15:22:18 +0700

    Redux Middlewares: Logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "redux/configureStore";
 
+store.dispatch({ type: "HELLO" });
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,7 +2,14 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
 
+const env = process.env.NODE_ENV;
+
 const middlewares = [thunk];
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
   users
