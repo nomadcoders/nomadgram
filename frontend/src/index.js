a654f34879c7891d41a62ed7b16b0f89e1666c39
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-10-31 15:17:36 +0700

    Redux Middlewares: Thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import configureStore from "redux/configureStore";
-
-const store = configureStore();
+import store from "redux/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import users from "redux/modules/users";
+
+const middlewares = [thunk];
 
 const reducer = combineReducers({
   users
 });
 
-let configureStore = initialState => createStore(reducer);
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
-export default configureStore;
+export default store();
