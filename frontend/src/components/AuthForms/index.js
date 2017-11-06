7db974bb5325faed2a06cc2a6e2d005ede3f2a68
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-06 23:19:15 +0700

    LoginForm

--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -3,35 +3,47 @@ import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
 export const LoginForm = props => (
-  <div>
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Log in" />
+  <div className={styles.formComponent}>
+    <form className={styles.form}>
+      <input type="text" placeholder="Username" className={styles.textInput} />
+      <input
+        type="password"
+        placeholder="Password"
+        className={styles.textInput}
+      />
+      <input type="submit" value="Log in" className={styles.button} />
     </form>
-    <span>or</span>
-    <span>
+    <span className={styles.divider}>or</span>
+    <span className={styles.facebookLink}>
       <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
       with Facebook
     </span>
-    <span>Forgot password?</span>
+    <span className={styles.forgotLink}>Forgot password?</span>
   </div>
 );
 
 export const SignupForm = props => (
-  <div>
+  <div className={styles.formComponent}>
     <h3>Sign up to see photos and videos from your friends.</h3>
-    <button>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
-      with Facebook
+    <button className={styles.button}>
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> Log in with
+      Facebook
     </button>
-    <span>or</span>
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Full Name" />
-      <input type="username" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Sign up" />
+    <span className={styles.divider}>or</span>
+    <form className={styles.form}>
+      <input type="email" placeholder="Email" className={styles.textInput} />
+      <input type="text" placeholder="Full Name" className={styles.textInput} />
+      <input
+        type="username"
+        placeholder="Username"
+        className={styles.textInput}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className={styles.textInput}
+      />
+      <input type="submit" value="Sign up" className={styles.button} />
     </form>
     <p>
       By signing up, you agree to our <span>Terms & Privacy Policy</span>.
