ffaef622ccad399b05bb85abd85bc5196e0734fa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-07 14:07:33 +0700

    Enabled Import / Export of translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,21 @@
 export const translations = {
-  es: {
-    "About Us": "Sobre Nosotros"
-  }
-};
+  'es': {
+    'Have an account?': 'Tienes una cuenta?',
+    'Log in': 'Iniciar Sesión',
+    'Don\'t have an account?': 'No tienes una cuenta?',
+    'Sign up': '',
+    'Get the app': '',
+    'About Us': '',
+    'Support': '',
+    'Blog': '',
+    'Press': '',
+    'API': '',
+    'Jobs': '',
+    'Privacy': '',
+    'Terms': '',
+    'Directory': '',
+    'Language': '',
+  },
+  'options': {
+  },
+}
