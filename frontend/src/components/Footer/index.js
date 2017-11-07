8cb3dc939e8e80d0c7240d30ae929b3996ba9454
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-07 13:47:08 +0700

    Translating the footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
 const Footer = (props, context) => (
@@ -6,16 +7,16 @@ const Footer = (props, context) => (
     <div className={styles.column}>
       <nav className={styles.nav}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>About Us</li>
-          <li className={styles.listItem}>Support</li>
-          <li className={styles.listItem}>Blog</li>
-          <li className={styles.listItem}>Press</li>
-          <li className={styles.listItem}>API</li>
-          <li className={styles.listItem}>Jobs</li>
-          <li className={styles.listItem}>Privacy</li>
-          <li className={styles.listItem}>Terms</li>
-          <li className={styles.listItem}>Directory</li>
-          <li className={styles.listItem}>Language</li>
+          <li className={styles.listItem}>{context.t("About Us")}</li>
+          <li className={styles.listItem}>{context.t("Support")}</li>
+          <li className={styles.listItem}>{context.t("Blog")}</li>
+          <li className={styles.listItem}>{context.t("Press")}</li>
+          <li className={styles.listItem}>{context.t("API")}</li>
+          <li className={styles.listItem}>{context.t("Jobs")}</li>
+          <li className={styles.listItem}>{context.t("Privacy")}</li>
+          <li className={styles.listItem}>{context.t("Terms")}</li>
+          <li className={styles.listItem}>{context.t("Directory")}</li>
+          <li className={styles.listItem}>{context.t("Language")}</li>
         </ul>
       </nav>
     </div>
@@ -24,5 +25,9 @@ const Footer = (props, context) => (
     </div>
   </footer>
 );
+
+Footer.contextTypes = {
+  t: PropTypes.func.isRequired
+};
 
 export default Footer;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,5 @@
 export const translations = {
   es: {
-    "Log in": "Iniciar Sesión"
+    "About Us": "Sobre Nosotros"
   }
 };
