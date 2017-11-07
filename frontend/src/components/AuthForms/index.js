722a9533d853859b417f271a63eb98dfd7caeb84
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-07 14:25:40 +0700

    Enabled i18n on Authforms

--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -1,54 +1,84 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
-export const LoginForm = props => (
+export const LoginForm = (props, context) => (
   <div className={styles.formComponent}>
     <form className={styles.form}>
-      <input type="text" placeholder="Username" className={styles.textInput} />
       <input
-        type="password"
-        placeholder="Password"
+        type="text"
+        placeholder={context.t("Username")}
         className={styles.textInput}
       />
-      <input type="submit" value="Log in" className={styles.button} />
+      <input
+        type="password"
+        placeholder={context.t("Password")}
+        className={styles.textInput}
+      />
+      <input
+        type="submit"
+        value={context.t("Log in")}
+        className={styles.button}
+      />
     </form>
-    <span className={styles.divider}>or</span>
+    <span className={styles.divider}>{context.t("or")}</span>
     <span className={styles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
-      with Facebook
+      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />{" "}
+      {context.t("Log in with Facebook")}
     </span>
-    <span className={styles.forgotLink}>Forgot password?</span>
+    <span className={styles.forgotLink}>{context.t("Forgot password?")}</span>
   </div>
 );
 
-export const SignupForm = props => (
+LoginForm.contextTypes = {
+  t: PropTypes.func.isRequired
+};
+
+export const SignupForm = (props, context) => (
   <div className={styles.formComponent}>
     <h3 className={styles.signupHeader}>
-      Sign up to see photos and videos from your friends.
+      {context.t("Sign up to see photos and videos from your friends.")}
     </h3>
     <button className={styles.button}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> Log in with
-      Facebook
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
+      {context.t("Log in with Facebook")}
     </button>
     <span className={styles.divider}>or</span>
     <form className={styles.form}>
-      <input type="email" placeholder="Email" className={styles.textInput} />
-      <input type="text" placeholder="Full Name" className={styles.textInput} />
+      <input
+        type="email"
+        placeholder={context.t("Email")}
+        className={styles.textInput}
+      />
+      <input
+        type="text"
+        placeholder={context.t("Full Name")}
+        className={styles.textInput}
+      />
       <input
         type="username"
-        placeholder="Username"
+        placeholder={context.t("Username")}
         className={styles.textInput}
       />
       <input
         type="password"
-        placeholder="Password"
+        placeholder={context.t("Password")}
         className={styles.textInput}
       />
-      <input type="submit" value="Sign up" className={styles.button} />
+      <input
+        type="submit"
+        value={context.t("Sign up")}
+        className={styles.button}
+      />
     </form>
     <p className={styles.terms}>
-      By signing up, you agree to our <span>Terms & Privacy Policy</span>.
+      {context.t("By signing up, you agree to our ")}
+      <span>{context.t("Terms & Privacy Policy")}</span>.
     </p>
   </div>
 );
+
+SignupForm.contextTypes = {
+  t: PropTypes.func.isRequired
+};
