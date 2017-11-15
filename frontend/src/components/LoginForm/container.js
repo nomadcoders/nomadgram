f9c84645c51a96bd3d6bc01f3d54584bd84d6f20
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-15 10:19:41 +0700

    Controlling inputs and submit on LoginForm

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginForm from "./presenter";
 
-const Container = props => <LoginForm {...props} />;
+class Container extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  render() {
+    const { username, password } = this.state;
+    return (
+      <LoginForm
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        usernameValue={username}
+        passwordValue={password}
+      />
+    );
+  }
+  _handleInputChange = event => {
+    const { target: { value, name } } = event;
+    this.setState({
+      [name]: value
+    });
+  };
+  _handleSubmit = event => {
+    event.preventDefault();
+  };
+}
 
 export default Container;
--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -5,16 +5,22 @@ import formStyles from "shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="text"
         placeholder={context.t("Username")}
         className={formStyles.textInput}
+        onChange={props.handleInputChange}
+        name="username"
+        value={props.usernameValue}
       />
       <input
         type="password"
         placeholder={context.t("Password")}
         className={formStyles.textInput}
+        onChange={props.handleInputChange}
+        name="password"
+        value={props.passwordValue}
       />
       <input
         type="submit"
@@ -32,6 +38,13 @@ const LoginForm = (props, context) => (
     </span>
   </div>
 );
+
+LoginForm.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 LoginForm.contextTypes = {
   t: PropTypes.func.isRequired
