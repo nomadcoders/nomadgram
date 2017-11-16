caa62d565eb41712b0dff7e175e6c9f5b6c5745b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-16 16:35:21 +0700

    Getting the token

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -93,7 +93,8 @@ function createAccount(username, password, email, name) {
 // initial state
 
 const initialState = {
-  isLoggedIn: localStorage.getItem("jwt") ? true : false
+  isLoggedIn: localStorage.getItem("jwt") ? true : false,
+  token: localStorage.getItem("jwt")
 };
 
 // reducer
