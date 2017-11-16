95c599c4d4772192c69366ef0316e361c01b6920
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-16 21:02:39 +0700

    Making textarea fit content

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentBox = (props, context) => (
   <form className={styles.commentBox}>
-    <textarea
+    <Textarea
       className={styles.input}
       placeholder={context.t("Add a comment...")}
     />
