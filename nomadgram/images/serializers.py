9753ce66fb7e3fd4ffda434f33f947f1748f3df7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-16 21:30:30 +0700

    Added 'is_liked' to ImageSerializer

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -63,6 +63,7 @@ class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
     creator = FeedUserSerializer()
     tags = TagListSerializerField()
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Image
@@ -75,8 +76,19 @@ class ImageSerializer(TaggitSerializer, serializers.ModelSerializer):
             'like_count',
             'creator',
             'tags',
-            'natural_time'
+            'natural_time',
+            'is_liked'
         )
+    
+    def get_is_liked(self, obj):
+        if 'request' in self.context:
+            request = self.context['request']
+            try:
+                models.Like.objects.get(creator__id=request.user.id, image__id=obj.id)
+                return True
+            except models.Like.DoesNotExist:
+                return False
+        return False
 
 
 class LikeSerializer(serializers.ModelSerializer):
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -34,7 +34,7 @@ class Images(APIView):
         sorted_list = sorted(
             image_list, key=lambda image: image.created_at, reverse=True)
 
-        serializer = serializers.ImageSerializer(sorted_list, many=True)
+        serializer = serializers.ImageSerializer(sorted_list, many=True, context={'request': request})
 
         return Response(serializer.data)
 
@@ -216,7 +216,7 @@ class ImageDetail(APIView):
         except models.Image.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = serializers.ImageSerializer(image)
+        serializer = serializers.ImageSerializer(image, context={'request': request})
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
