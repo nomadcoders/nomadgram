0f282a75ecb9e5e3b091db74caaa3ed3e3dae7a7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-16 17:53:25 +0700

    FeedPhoto Component pt. Three

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
 import PhotoActions from "components/PhotoActions";
+import PhotoComments from "components/PhotoComments";
 
 const FeedPhoto = (props, context) => {
   return (
@@ -19,6 +20,11 @@ const FeedPhoto = (props, context) => {
       <img src={props.file} alt={props.caption} />
       <div>
         <PhotoActions number={props.like_count} />
+        <PhotoComments
+          caption={props.caption}
+          creator={props.creator.username}
+          comments={props.comments}
+        />
       </div>
     </div>
   );
---
+++ frontend/src/components/PhotoComments/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
+
+const PhotoComments = props => (
+  <div>
+    <ul>
+      <Comment username={props.creator} comment={props.caption} />
+      {props.comments.map(comment => (
+        <Comment
+          username={comment.creator.username}
+          comment={comment.message}
+          key={comment.id}
+        />
+      ))}
+    </ul>
+  </div>
+);
+
+const Comment = props => (
+  <li>
+    <span>{props.username}</span> <span>{props.comment}</span>
+  </li>
+);
+
+PhotoComments.propTypes = {
+  caption: PropTypes.string.isRequired,
+  creator: PropTypes.string.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      creator: PropTypes.shape({
+        profile_image: PropTypes.string,
+        username: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired
+};
+
+export default PhotoComments;
