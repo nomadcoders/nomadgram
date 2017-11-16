8be1dac05e91f11c4fe57216f03c706e23d2118a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-16 23:51:14 +0700

    Liking and Unliking images

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -5,6 +5,8 @@ import { actionCreators as userActions } from "redux/modules/user";
 // actions
 
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const UNLIKE_PHOTO = "UNLIKE_PHOTO";
 
 // action creators
 
@@ -12,6 +14,20 @@ function setFeed(feed) {
   return {
     type: SET_FEED,
     feed
+  };
+}
+
+function doLikePhoto(photoId) {
+  return {
+    type: LIKE_PHOTO,
+    photoId
+  };
+}
+
+function doUnlikePhoto(photoId) {
+  return {
+    type: UNLIKE_PHOTO,
+    photoId
   };
 }
 
@@ -31,7 +47,45 @@ function getFeed() {
         }
         return response.json();
       })
-      .then(json => dispatch(setFeed(json)));
+      .then(json => {
+        dispatch(setFeed(json));
+      });
+  };
+}
+
+function likePhoto(photoId) {
+  return (dispatch, getState) => {
+    const { user: { token } } = getState();
+    fetch(`/images/${photoId}/likes/`, {
+      method: "POST",
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(userActions.logout());
+      } else if (response.ok) {
+        dispatch(doLikePhoto(photoId));
+      }
+    });
+  };
+}
+
+function unlikePhoto(photoId) {
+  return (dispatch, getState) => {
+    const { user: { token } } = getState();
+    fetch(`/images/${photoId}/unlikes/`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(userActions.logout());
+      } else if (response.ok) {
+        dispatch(doUnlikePhoto(photoId));
+      }
+    });
   };
 }
 
@@ -45,6 +99,10 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_FEED:
       return applySetFeed(state, action);
+    case LIKE_PHOTO:
+      return applyLikePhoto(state, action);
+    case UNLIKE_PHOTO:
+      return applyUnlikePhoto(state, action);
     default:
       return state;
   }
@@ -60,10 +118,36 @@ function applySetFeed(state, action) {
   };
 }
 
+function applyLikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: true, like_count: photo.like_count + 1 };
+    }
+    return photo;
+  });
+  return { ...state, feed: updatedFeed };
+}
+
+function applyUnlikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: false, like_count: photo.like_count - 1 };
+    }
+    return photo;
+  });
+  return { ...state, feed: updatedFeed };
+}
+
 // Exports
 
 const actionCreators = {
-  getFeed
+  getFeed,
+  likePhoto,
+  unlikePhoto
 };
 
 export { actionCreators };
