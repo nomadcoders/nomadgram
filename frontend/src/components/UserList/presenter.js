c0174e12050f93927ebd5149479d73d79f17676d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-18 19:04:02 +0700

    UserList CSS

--- frontend/src/components/UserList/presenter.js
+++ frontend/src/components/UserList/presenter.js
@@ -6,22 +6,19 @@ import Ionicon from "react-ionicons";
 
 const UserList = props => (
   <div className={styles.container}>
-    <div className={styles.overlay} />
     <div className={styles.box}>
       <header className={styles.header}>
-        <h4>{props.title}</h4>
+        <h4 className={styles.title}>{props.title}</h4>
         <Ionicon icon="md-close" fontSize="20px" color="black" />
       </header>
       <div className={styles.content}>{props.loading ? <Loading /> : null}</div>
     </div>
   </div>
 );
-
-const LoadingUserList = props =>
-  (UserList.propTypes = {
-    title: PropTypes.string.isRequired,
-    loading: PropTypes.bool.isRequired,
-    users: PropTypes.array
-  });
+UserList.propTypes = {
+  title: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  users: PropTypes.array
+};
 
 export default UserList;
