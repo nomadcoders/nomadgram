d7c0ca3d79328a3dc2b5d6d5993c5767778b5727
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-18 16:39:00 +0700

    Controlling CommentBox

--- frontend/src/components/CommentBox/container.js
+++ frontend/src/components/CommentBox/container.js
@@ -2,9 +2,34 @@ import React, { Component } from "react";
 import CommentBox from "./presenter";
 
 class Container extends Component {
+  state = {
+    comment: ""
+  };
   render() {
-    return <CommentBox />;
+    return (
+      <CommentBox
+        {...this.state}
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        handleKeyPress={this._handleKeyPress}
+      />
+    );
   }
+  _handleInputChange = event => {
+    const { target: { value } } = event;
+    this.setState({
+      comment: value
+    });
+  };
+  _handleSubmit = () => {
+    console.log(this.state);
+  };
+  _handleKeyPress = event => {
+    const { key } = event;
+    if (key === "Enter") {
+      event.preventDefault();
+    }
+  };
 }
 
 export default Container;
--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -4,13 +4,23 @@ import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentBox = (props, context) => (
-  <form className={styles.commentBox}>
+  <form className={styles.commentBox} onSubmit={props.handleSubmit}>
     <Textarea
       className={styles.input}
       placeholder={context.t("Add a comment...")}
+      onChange={props.handleInputChange}
+      value={props.comment}
+      onKeyPress={props.handleKeyPress}
     />
   </form>
 );
+
+CommentBox.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  comment: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleKeyPress: PropTypes.func.isRequired
+};
 
 CommentBox.contextTypes = {
   t: PropTypes.func.isRequired
