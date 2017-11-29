710ad31335238b421621d3685ecfb9c2becffc30
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-11-29 15:04:05 +0700

    Context to serializers

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -175,14 +175,14 @@ class Search(APIView):
             images = models.Image.objects.filter(
                 tags__name__in=hashtags).distinct()
 
-            serializer = serializers.ImageSerializer(images, many=True)
+            serializer = serializers.ImageSerializer(images, many=True, context={'request': request})
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         else:
 
             images = models.Image.objects.all()[:20]
-            serializer = serializers.ImageSerializer(images, many=True)
+            serializer = serializers.ImageSerializer(images, many=True, context={'request': request})
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
