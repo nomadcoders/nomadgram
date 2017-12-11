33071b1060953090b07b7cb26ebfb590b682a5ca
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-11 18:26:05 +0700

    DB

--- config/settings/production.py
+++ config/settings/production.py
@@ -134,7 +134,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 
 # Use the Heroku-style specification
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-# DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES['default'] = env.db('DATABASE_URL')
 
 # CACHING
 # ------------------------------------------------------------------------------
