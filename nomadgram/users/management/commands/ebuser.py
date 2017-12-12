7028859aaac0e3788c0295a0d10996063c83dfae
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-12 15:35:00 +0700

    Superuser

--- nomadgram/users/management/commands/ebuser.py
+++ nomadgram/users/management/commands/ebuser.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+
         User = get_user_model()
         User.objects.create_superuser('admin', 'admin@example.com', 'pass')
