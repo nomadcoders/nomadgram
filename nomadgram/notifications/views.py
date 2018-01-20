648ea369970602af619313055326123c43a0c5dd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-20 14:23:51 +0700

    Sending push notificationswith small timeout

--- nomadgram/notifications/views.py
+++ nomadgram/notifications/views.py
@@ -1,3 +1,6 @@
+import requests
+import json
+import time
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -28,4 +31,35 @@ def create_notification(creator, to, notification_type, image=None, comment=None
         comment=comment
     )
 
+    time.sleep(10)
+
     notification.save()
+
+    action = ''
+
+    if notification_type == 'like':
+
+        action = 'liked your photo'
+    
+    elif notification_type == 'comment':
+
+        action = 'commented on your photo'
+    
+    elif notification_type == 'follow':
+
+        action = 'followed you'
+        
+
+    url = "https://exp.host/--/api/v2/push/send"
+    data = {
+        "to": creator.push_token,
+        "sound": "default",
+        "body": f'Somebody {action}',
+        "badge": 1
+    }
+    headers = {'Content-type': 'application/json',
+               'Accept': 'application/json', 'Accept-Encoding': 'gzip, deflate'}
+
+    request = requests.post(url, data=json.dumps(data), headers=headers)
+
+    print(request.json())
